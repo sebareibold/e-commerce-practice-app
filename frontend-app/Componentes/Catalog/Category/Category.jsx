9a575ac532601f14979943959da1344a5a57e12e
sebareibold <sebastianreibold2003@gmail.com>
2025-03-03 14:56:21 -0300

    Avances en Catalogo

---
+++ frontend-app/Componentes/Catalog/Category/Category.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { Pressable, View, Text, StyleSheet, Dimensions, Image, Alert } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+const Category = ({ setCategoriaSeleccionada }) => {
+    const [categoriaSeleccionada, setCategoria] = useState(null);
+
+    const handleCategoryPress = (category) => {
+        setCategoriaSeleccionada(category);
+        setCategoria(category); // Actualiza el estado
+        Alert.alert("Categoría seleccionada", `Has seleccionado: ${category}`); 
+    };
+
+    const handleVerTodasPress = () => {
+        setCategoriaSeleccionada(null);
+        setCategoria(null)
+        Alert.alert("Sin categoria"); 
+    }
+
+    return (
+        <View style={{ flexDirection: 'column' }}>
+            {/* Títulos */}
+            <View style={styles.textContainer}>
+                <Text style={styles.categoria}>Categoría</Text>
+                <Pressable onPress={handleVerTodasPress}>
+                    <Text style={styles.verTodas}>Ver todas</Text>
+                </Pressable>          
+            </View>
+
+            {/* Botones de categorías */}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginRight: 14 }}>
+                {/* Remeras */}
+                <Pressable style={[styles.button, categoriaSeleccionada === "Remeras" && styles.activo]} onPress={() => handleCategoryPress("Remeras")}>
+                    <Image source={require("../../../assets/images/tshirt (1).png")} style={styles.image} />
+                </Pressable>
+                {/* Buzos */}
+                <Pressable style={[styles.button, categoriaSeleccionada === "Buzos" && styles.activo]} onPress={() => handleCategoryPress("Buzos")}>
+                    <Image source={require("../../../assets/images/hoodie (1).png")} style={styles.image} />
+                </Pressable>
+                {/* Pantalones */}
+                <Pressable style={[styles.button, categoriaSeleccionada === "Pantalones" && styles.activo]} onPress={() => handleCategoryPress("Pantalones")}>
+                    <Image source={require("../../../assets/images/pants.png")} style={styles.image} />
+                </Pressable>
+                {/* Zapatillas */}
+                <Pressable style={[styles.button, categoriaSeleccionada === "Zapatillas" && styles.activo]} onPress={() => handleCategoryPress("Zapatillas")}>
+                    <Image source={require("../../../assets/images/shoes (1).png")} style={styles.image} />
+                </Pressable>
+            </View>
+        </View>
+    );
+};
+
+export default Category;
+
+const styles = StyleSheet.create({
+    image: {
+        width: width * 0.08,
+        height: width * 0.08,
+        resizeMode: 'contain',
+    },
+    textContainer: {
+        marginLeft: width * 0.05,
+        marginRight: width * 0.05,
+        marginBottom: width * 0.02,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    categoria: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+    verTodas: {
+        fontSize: 14,
+        color: '#666',
+    },
+    button: {
+        backgroundColor: '#0f1626',
+        borderRadius: 20,
+        width: width * 0.14,
+        aspectRatio: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: width * 0.05,
+        marginRight: width * 0.02,
+    },
+    activo: {
+        backgroundColor: '#f7f8fa',
+        elevation: 1,
+        borderColor: '#1c1c1c',
+        borderWidth: 1,       // Ancho de borde de 1px
+    }
+    
+});
--- frontend-app/Componentes/Catalog/Item/Item.jsx
+++ frontend-app/Componentes/Catalog/Item/Item.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, Image, TouchableOpacity, Dimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+const { width, height } = Dimensions.get('window'); // Obtener las dimensiones de la pantalla
 
-const Item = () => {
+
+const Item = ({ item }) => {  // item es una prop que contiene los datos del artículo
   const navigation = useNavigation();
-  const item = { id: 9 };
 
   return (
-    <TouchableOpacity 
-      style={styles.item} 
-      onPress={() => navigation.navigate('ItemDetail', { itemId: item.id })}
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() => navigation.navigate('ItemDetail', { itemId: item.id })} 
     >
       <Image
-        source={require("../../assets/images/reme.png")}
+        source={{ uri: item.image }}
         style={styles.image}
       />
       <View style={styles.secondSideItem}>
-        <Text style={styles.text}>Remera Overside</Text>
+        <Text style={styles.text}>{item.title}</Text>  
       </View>
       <View style={styles.otherSideItem}>
-        <Text style={styles.textPrice}>$40.000</Text>
+        <Text style={styles.textPrice}>${item.price}</Text> 
+        <Text style={styles.textPrice}>{item.rating.rate}</Text> 
       </View>
     </TouchableOpacity>
+
   );
 };
 
@@ -44,21 +47,31 @@ const styles = StyleSheet.create({
   image: {
     marginTop: '5%',
     marginLeft: '5%',
-    width: 120,
-    height: 120,
+    maxWidth: 100,  // Tamaño máximo permitido
+    maxHeight: 100, // Tamaño máximo permitido
+    width: '100%',  // Ajuste dinámico
+    aspectRatio: 1, // Mantiene la proporción original
+    resizeMode: 'contain', // Asegura que la imagen se ajuste sin distorsión
   },
+
   secondSideItem: {
     flex: 1, // Ocupar todo el espacio restante
     marginLeft: '10%',
   },
-  otherSideItem: {},
+  otherSideItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between', // Esto separa los elementos al máximo dentro del View
+    alignItems: 'center', // Alinea verticalmente los textos
+    width: '90%', // Asegura que ocupe todo el ancho disponible
+    paddingHorizontal: 10, // Agrega algo de espacio a los lados
+  },
   textPrice: {
     fontSize: 13,
     marginBottom: '10%', // Espaciado entre textos
   },
   text: {
     color: '#686868',
-    fontSize: 16,
+    fontSize: 12,
     marginBottom: '2%', // Espaciado entre textos
   },
 });
