19b886f0edc4de8c76dd6eb861bab4108ba5c3df
sebareibold <sebastianreibold2003@gmail.com>
2025-03-02 21:06:32 -0300

    Reestructuracion de los directorios

--- frontend-app/app/(tabs)/productos.tsx
+++ frontend-app/app/(tabs)/productos.tsx
@@ -1,13 +1,13 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import ItemsContainer from '../../Componentes/ItemsContainer/ItemsContainer';
-import ItemDetail from '../../Componentes/ItemDetail/ItemDetail';
+import ItemListContainer from '../../Componentes/Catalog/ItemListContainer/ItemListContainer';
+import ItemDetail from '../../Componentes/Catalog/ItemDetail/ItemDetail';
 
 const Stack = createStackNavigator();
 
 export default function VistaProductos() {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="ListaProductos" component={ItemsContainer} />
+            <Stack.Screen name="ListaProductos" component={ItemListContainer} />
             <Stack.Screen name="ItemDetail" component={ItemDetail} />
         </Stack.Navigator>
     );
