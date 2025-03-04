8dfd4dd0770c2ef1a6bd401475b4cceff654e6d0
Bautista Fernandez <bautista.fernandez@est.fi.uncoma.edu.ar>
2025-03-04 20:17:50 -0300

    actualizacion home

--- frontend-app/Componentes/Home/HomeContainer/HomeContainer.jsx
+++ frontend-app/Componentes/Home/HomeContainer/HomeContainer.jsx
@@ -1,9 +1,26 @@
+import { ImageBackground } from 'expo-image';
 import React from 'react'
+import { View, StyleSheet, Text, ScrollView, Image } from 'react-native';
 
 const HomeContainer = () => {
   return (
-    <div>Hola 2</div>
-  )
+    <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={styles.brandTitle}>King Of The South</Text>  
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  brandTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 50,
+  },
+});
 
 export default HomeContainer
